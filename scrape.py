b7f69a7e3cd6c3b1d60ce478b86e20b605da11e9
Sean Cronin <seancron@gmail.com>
2010-10-06 22:29:06 -0400

    Initial Commit

---
+++ scrape.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from BeautifulSoup import BeautifulSoup
+import urllib2
+
+bookmarks = open("bookmarks.html", "w")
+bookmarks.write("""
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1>Bookmarks</H1>
+<DL><p>
+""")
+
+urls = open("urls.txt", "r")
+
+for url in urls:
+    while True:
+        try:
+            page = urllib2.urlopen(url)
+            soup = BeautifulSoup(page)
+            allStories = soup.findAll("h2", {"class" : "item-title"})
+            allLinks = [tag.div.a for tag in allStories]
+
+            for link in allLinks:
+                bookmarks.write('<DT>' + str(link) + '\n')
+
+            url = soup.find("div", {"id" : "more"}).a['href']
+        except:
+            break
+
+bookmarks.write("""
+</DL><p>""")
